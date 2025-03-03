0f03be556c1dfd43350cd05a613a3fddc85023d9
JabedOmar11 <jabedomar472@gmail.com>
2025-03-03 22:26:44 +0600

    finishing

---
+++ script/new.js
@@ -0,0 +1,40 @@
+document.getElementById("box-1").
+addEventListener('click', function(){
+alert('Board Uploaded Successfully ')
+
+})
+
+// box-2
+document.getElementById("box-2").
+addEventListener('click', function(){
+alert('Board Uploaded Successfully ')
+
+})
+
+// box-3
+document.getElementById("box-3").
+addEventListener('click', function(){
+alert('Board Uploaded Successfully ')
+
+})
+
+// box-4
+document.getElementById("box-4").
+addEventListener('click', function(){
+alert('Board Uploaded Successfully ')
+
+})
+
+// box-5
+document.getElementById("box-5").
+addEventListener('click', function(){
+alert('Board Uploaded Successfully ')
+
+})
+
+// box-6
+document.getElementById("box-6").
+addEventListener('click', function(){
+alert('Board Uploaded Successfully ')
+
+})
